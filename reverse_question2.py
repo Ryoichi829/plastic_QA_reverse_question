989ca5261b5241019a315ae94dd89a4d50f6705a
Ryoichi829 <110223210+Ryoichi829@users.noreply.github.com>
2024-06-22 20:19:48 +0900

    Update reverse_question2.py

--- reverse_question2.py
+++ reverse_question2.py
@@ -46,7 +46,10 @@ def generate_final_answer_chain(user_question, reverse_questions, answers):
         "answer2": answers[1]
     })
 
-
+def update_ans1():
+    st.session_state.reverse_answer1 = reverse_answer1
+    return
+    
 st.title("プラスチック Ｑ＆Ａ")
 st.write("gpt-4oを使ったチャットボット　―質問返し―")
 
@@ -70,11 +73,11 @@ if (question) and (question != st.session_state.user_question):
 
 if len(st.session_state.reverse_questions) >= 2:
     # 逆質問を表示し、ユーザの回答を取得
-    answer1 = st.text_input(f"逆質問 {st.session_state.reverse_questions[0]}", key="reverse_answer1")
+    reverse_answer1 = st.text_input(f"逆質問 {st.session_state.reverse_questions[0]}", key="reverse_answer1", on_change=update_ans1)
     reverse_answer2 = st.text_input(f"逆質問 {st.session_state.reverse_questions[1]}", key="reverse_answer2")
 
-    if answer1 and reverse_answer2:
-        st.session_state.reverse_answer1 = answer1
+    if reverse_answer1 and reverse_answer2:
+        # st.session_state.reverse_answer1 = reverse_answer1
         st.session_state.reverse_answer2 = reverse_answer2
         # ユーザの回答を踏まえた最終回答を生成
         final_answer = generate_final_answer_chain(
